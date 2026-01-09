026f6d5497d930bc1621bcbbf6b6231d7b0e9d5e
Dedeke David Iteoluwakiisi <thatsaxydev@gmail.com>
2026-01-09 07:12:27 +0100

    Initial Commit

---
+++ FlowFocus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlowFocus
+//
+//  Created by Dedeke David Iteoluwakiisi on 09/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlowFocus/FlowFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowFocusApp.swift
+//  FlowFocus
+//
+//  Created by Dedeke David Iteoluwakiisi on 09/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlowFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
